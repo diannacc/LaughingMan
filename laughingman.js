ff904ce19b762d6caa941914f32903c31cc297ef
diannacc <diannacc@nyit.edu>
2025-11-19 10:39:01 -0500

    Started loop for children objects

--- laughingman.js
+++ laughingman.js
@@ -1,3 +1,6 @@
+const SQUARE_COUNT = 3;
+const SPEED = 5;
+const TIMER_SPEED = 100;
 
 document.addEventListener('DOMContentLoaded', ()=>{
     let squareElement = document.getElementById("square");
@@ -5,5 +8,23 @@ document.addEventListener('DOMContentLoaded', ()=>{
     squareElement.addEventListener("click", ()=>{
         alert("OMG YOU CAUGHT ME!");
     });
+    let box = document.querySelector('#box');
 
+    for(let i = 0; i<SQUARE_COUNT; i++){
+        //create the square
+        let square = document.createElement('img');
+        square.src = 'laughing_man.jpg';
+        square.alt = 'Catch the Laughing Man!';
+        square.className = 'square';
+        box.appendChild(square);
+        //add it to the screen
+    }
+    let allSquares = box.children;
+    let squareArray = Array.from(children);
+    squareArray.forEach(function(element,index){
+        var dx = SPEED * (Math.random()*2-1);
+        var dy = SPEED * (Math.random()*2-1);
+    });
+    
 });
+
