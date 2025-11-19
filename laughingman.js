dedfbaca41c3caadba05dec97b87541279fa2f13
diannacc <diannacc@nyit.edu>
2025-11-19 10:12:02 -0500

    Added alert on square click

---
+++ laughingman.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let squareElement = document.getElementById("square");
+    //let squareElement = document.getElementById("#square");
+    squareElement.addEventListener("click", ()=>{
+        alert("OMG YOU CAUGHT ME!");
+    });
+
+});
