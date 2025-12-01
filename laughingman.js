4484cc38e017651a28ca27cd46911399501407d3
diannacc <diannacc@nyit.edu>
2025-12-01 14:58:55 -0500

    Added:
    Add code so that when we mouseover an img it's src changes to the attached Orochimaru.jpgorochimaru_50x50.jpg / Mouse out should set it back to the original image

--- laughingman.js
+++ laughingman.js
@@ -1,5 +1,5 @@
-const SQUARE_COUNT = 3;
-const SPEED = 5;
+const SQUARE_COUNT = 30;
+const SPEED = 10;
 const TIMER_SPEED = 100;
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -20,11 +20,48 @@ document.addEventListener('DOMContentLoaded', ()=>{
         //add it to the screen
     }
     let allSquares = box.children;
-    let squareArray = Array.from(children);
+    let squareArray = Array.from(allSquares);
     squareArray.forEach(function(element,index){
+
+        //#5 Adding the hover feature to show the image / not show it
+        element.addEventListener("mouseover", ()=> {
+            element.dataset.hovering = element.src;
+            element.src = "Orochimaru.jpg"; //Hover sets to new IMG to orochimaru
+        });
+
+        element.addEventListener("mouseout", ()=>{
+            element.src = "laughing_man.jpg"; //Not Hovering resets IMG to laughing_man
+        });
+
         var dx = SPEED * (Math.random()*2-1);
         var dy = SPEED * (Math.random()*2-1);
+        setInterval(()=> {
+            let currentLeft = parseInt(element.style.left) || 275;
+            let currentTop = parseInt(element.style.top) || 175;
+            if(currentLeft >=450 || currentLeft<=0){
+                dx*=-1;
+                element.style.borderColor = getRandomColor();
+            }
+            if(currentTop >=350 || currentTop <=0){
+                dy*=-1;
+                element.style.borderColor = getRandomColor();
+            }
+
+
+            element.style.left = (currentLeft+dx)+"px";
+            element.style.top = (currentTop+dy)+"px";
+        }, TIMER_SPEED);
     });
     
+
 });
 
+function getRandomColor(){
+    let r = parseInt(Math.random()*256);
+    let g = parseInt(Math.random()*256);
+    let b = parseInt(Math.random()*256);
+    let color = 'rgb(${r},${g},${b})';
+    return color;
+}
+
+//FOR HW DO #5-#6, #1-4 DID IN CLASS ALREADY
